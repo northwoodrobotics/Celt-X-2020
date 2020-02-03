2e05a6467146ea26bc83efd4adb781336d04289c
cyberx86 <chripto25@yahoo.ca>
2020-02-02 22:05:29 -0500

    Add intake setters and intake constants

--- src/main/java/frc/team5406/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team5406/robot/subsystems/IntakeSubsystem.java
@@ -7,15 +7,84 @@
 
 package frc.team5406.robot.subsystems;
 
+import frc.team5406.robot.Constants;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.ControlType;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.*;
+
+
 public class IntakeSubsystem extends SubsystemBase {
-  /**
-   * Creates a new ExampleSubsystem.
-   */
+  
+  private static CANSparkMax intakeRollers = new CANSparkMax(Constants.INTAKE_ROLLER_MOTOR, MotorType.kBrushless);
+  private static CANSparkMax hopperBrushes = new CANSparkMax(Constants.HOPPER_BRUSH_MOTOR, MotorType.kBrushless);
+  private static TalonSRX throatSerializer = new TalonSRX(Constants.THROAT_SERIALIZER_MOTOR);
+
+
   public IntakeSubsystem() {
+    setupMotors();
+  }
+
+  private void setupMotors(){
+    intakeRollers.setSmartCurrentLimit(Constants.NEO550_CURRENT_LIMIT);
+    hopperBrushes.setSmartCurrentLimit(Constants.NEO550_CURRENT_LIMIT);
+    throatSerializer.enableCurrentLimit(true);
+    throatSerializer.configContinuousCurrentLimit(Constants.BAG_CURRENT_LIMIT);
+    throatSerializer.configPeakCurrentLimit(Constants.BAG_CURRENT_LIMIT);
+    throatSerializer.configPeakCurrentDuration(Constants.PEAK_CURRENT_DURATION);
 
   }
+
+  private void setIntakeRollerOutput(double output){
+    intakeRollers.set(output);
+  }
+
+  private void setHopperBrushOutput(double output){
+    hopperBrushes.set(output);
+  }
+
+  private void setSerializerOutput(double output){
+    throatSerializer.set(ControlMode.PercentOutput, output);
+  }
+
+  private void setIntakePosition(boolean out){
+
+  }
+
+  public void retractIntake(){
+    setIntakePosition(Constants.INTAKE_RETRACT);
+  }
+
+  public void extendIntake(){
+    setIntakePosition(Constants.INTAKE_EXTEND);
+  }
+
+  public void intake(){
+    setIntakeRollerOutput(Constants.INTAKE_ROLLER_OUTPUT);
+    setHopperBrushOutput(Constants.HOPPER_BRUSH_OUTPUT);
+  }
+
+  public void serialize(){
+    setSerializerOutput(Constants.HOPPER_BRUSH_OUTPUT);
+  }
+
+  public void outtake(){
+    setIntakeRollerOutput(-1*Constants.INTAKE_ROLLER_OUTPUT);
+  }
+
+  public void reverseSerializer(){
+    setSerializerOutput(-1*Constants.HOPPER_BRUSH_OUTPUT);
+  }
+
 
   @Override
   public void periodic() {
